c32959625f55f273ce0f93b62bda37a0b8d49e4f
Ali Manian <alimanian79@gmail.com>
2023-12-22 07:32:03 +0330

    add: RFM create by transactions file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,172 @@
-
-const userExcelFile = document.querySelector('#excel');
+// Excel File Upload By User
+const excelFile = document.querySelector('#excelFile');
+// After Select File Section
 const afterSelectFile = document.querySelector('#afterSelectFile');
+// Process Button
 const btnProcess = document.querySelector('#btnProcess');
+// Recency Column Select Options
 const recencySelect = document.querySelector('#recencySelect');
-const frequencySelect = document.querySelector('#frequencySelect');
+// Frequency Column Select Options
+const frequencySelect = document.querySelector('#frequencySelect') ?? undefined;
+// Monetary Column Select Options
 const monetarySelect = document.querySelector('#monetarySelect');
-const valuableSelect = document.querySelector('#valuableSelect');
-const sectionCount = document.querySelector('#sectionCount');
+// Valuable Column Select Option
+const valuableSelect = document.querySelector('#valuableSelect') ?? undefined;
+// Customer Id Column Select Option
+const idSelect = document.querySelector('#IdSelect') ?? undefined;
+// Count Input for Customer Segmentation
+const sectionCount = document.querySelector('#sectionCount') ?? undefined;
 
-const recencyScoreText = "بخش‌بندی R";
-const frequencyScoreText = "بخش‌بندی F";
-const monetaryScoreText = "بخش‌بندی M";
-const finalScoreText = "بخش‌بندی نهایی";
+// Generate Excel File Column Text
+const recencyScoreText = "Recency Score";
+const frequencyScoreText = "Frequency Score";
+const monetaryScoreText = "Monetary Score";
+const finalScoreText = "Final Score";
 
+// Change Valuable Select By Change Recency Select
 recencySelect.addEventListener('change', () => {
-    valuableSelect.selectedIndex = recencySelect.selectedIndex;
+    if(state === 'process')
+        valuableSelect.selectedIndex = recencySelect.selectedIndex;
 });
 
-userExcelFile.addEventListener('change', () => {
-    readExcelFileForGetHeaders(userExcelFile.files[0]).then(headers => {
-        fillSelectTags(headers);
+// Get and Fill Select Options By User Excel File Columns
+excelFile.addEventListener('change', () => {
+    readExcelFileForGetHeaders(excelFile.files[0]).then(headers => {
+        headers.forEach(function (h, i) {
+
+            let col = XLSX.utils.encode_col(i);
+
+            recencySelect.appendChild(
+                new Option(h, col)
+            );
+
+            monetarySelect.appendChild(
+                new Option(h, col)
+            );
+
+            if (frequencySelect !== undefined)
+                frequencySelect.appendChild(
+                    new Option(h, col)
+                );
+
+            if (valuableSelect !== undefined)
+                valuableSelect.appendChild(
+                    new Option(h, col)
+                );
+
+            if (idSelect !== undefined)
+                idSelect.appendChild(
+                    new Option(h, col)
+                );
+        });
     });
 
     afterSelectFile.style.display = 'flex';
 });
 
 btnProcess.addEventListener('click', () => {
-    // type true = asc, false = desc
-    sortExcelAndCreateNewExcel(userExcelFile.files[0],
-        recencySelect.options[recencySelect.selectedIndex].text,
-        frequencySelect.options[frequencySelect.selectedIndex].text,
-        monetarySelect.options[monetarySelect.selectedIndex].text,
-        valuableSelect.options[valuableSelect.selectedIndex].text).then(buffer => {
+    if (state === 'process') {
+        // type true = asc, false = desc
+        sortExcelAndCreateNewExcel(excelFile.files[0],
+            recencySelect.options[recencySelect.selectedIndex].text,
+            frequencySelect.options[frequencySelect.selectedIndex].text,
+            monetarySelect.options[monetarySelect.selectedIndex].text,
+            valuableSelect.options[valuableSelect.selectedIndex].text).then(buffer => {
 
-        // ایجاد لینک دانلود
-        const url = URL.createObjectURL(new Blob([buffer]));
-        document.getElementById("downloadExcel").href = url;
-        document.getElementById("downloadExcel").style.display = "block";
-    });
+            // ایجاد لینک دانلود
+            const url = URL.createObjectURL(new Blob([buffer]));
+            document.getElementById("downloadExcel").href = url;
+            document.getElementById("downloadExcel").style.display = "block";
+        });
+    } else if (state === 'create') {
+        createRFMExcelFile(excelFile.files[0],
+            idSelect.options[idSelect.selectedIndex].text,
+            recencySelect.options[recencySelect.selectedIndex].text,
+            monetarySelect.options[monetarySelect.selectedIndex].text).then(buffer => {
+
+            // ایجاد لینک دانلود
+            const url = URL.createObjectURL(new Blob([buffer]));
+            document.getElementById("downloadExcel").href = url;
+            document.getElementById("downloadExcel").style.display = "block";
+        });
+    }
 });
+
+function createRFMExcelFile(file,cID, recency, monetary) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+
+            const data = e.target.result;
+
+            // خواندن فایل
+            const workbook = XLSX.read(data, {type: 'binary'});
+            const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+            const rows = XLSX.utils.sheet_to_json(worksheet);
+
+            let customers = [];
+
+            rows.forEach(item => {
+
+                if (!customers[item[cID]]) {
+                    customers[item[cID]] = {
+                        "Customer ID": item[cID],
+                        Recency: 0,
+                        Frequency: 0,
+                        Monetary: 0,
+                        latestDate: 0
+                    };
+                }
+
+                const currentDate = parseInt(item[recency]);
+                customers[item[cID]].Monetary += item[monetary];
+                customers[item[cID]].Frequency++;
+
+                if (currentDate > customers[item[cID]].latestDate) {
+                    customers[item[cID]].latestDate = currentDate;
+                }
+            });
+
+            let maxDate = 0;
+            Object.keys(customers).forEach(cid => {
+                const latestDate = moment(customers[cid].latestDate, 'YYYYMMDD');
+                if (latestDate > maxDate) {
+                    maxDate = latestDate;
+                }
+            });
+
+            // اضافه کردن یک روز
+            maxDate.add(1, 'day');
+
+            Object.keys(customers).forEach(cid => {
+
+                const latestDate = moment(customers[cid].latestDate, 'YYYYMMDD');
+                const diff = maxDate.diff(latestDate, 'days');
+                customers[cid].Recency = diff;
+
+            });
+
+            customers.shift();
+            customers.forEach(customer => {
+                delete customer.latestDate;
+            });
+
+            const newWorkbook = XLSX.utils.book_new();
+
+            // تبدیل به ورکشیت و افزودن به ورک بوک جدید
+            const newWorksheet = XLSX.utils.json_to_sheet(customers);
+            XLSX.utils.book_append_sheet(newWorkbook, newWorksheet, "Customers");
+
+            // نوشتن ورک بوک جدید
+            const buffer = XLSX.write(newWorkbook, {bookType: 'xlsx', type: 'buffer'});
+
+            resolve(buffer);
+        }
+
+        reader.readAsArrayBuffer(file);
+    });
+
+}
 
 function sortExcelAndCreateNewExcel(file, recency, frequency, monetary, valuable) {
     return new Promise((resolve, reject) => {
@@ -66,7 +194,7 @@ function sortExcelAndCreateNewExcel(file, recency, frequency, monetary, valuable
             const rowsPerGroup = Math.floor(totalRows / sectionCount.value);
 
             // اضافه کردن ستون جدید
-            for(let i= 0; i < totalRows; i++) {
+            for (let i = 0; i < totalRows; i++) {
 
                 // محاسبه شماره گروه
                 let groupNum = sectionCount.value - Math.floor((i + 1) / rowsPerGroup);
@@ -81,7 +209,7 @@ function sortExcelAndCreateNewExcel(file, recency, frequency, monetary, valuable
                 return 0;
             });
 
-            for(let i= 0; i < totalRows; i++) {
+            for (let i = 0; i < totalRows; i++) {
                 // محاسبه شماره گروه
                 let groupNum = sectionCount.value - Math.floor((i + 1) / rowsPerGroup);
                 if (groupNum < 1) groupNum = 1;
@@ -96,7 +224,7 @@ function sortExcelAndCreateNewExcel(file, recency, frequency, monetary, valuable
                 return 0;
             });
 
-            for(let i= 0; i < totalRows; i++) {
+            for (let i = 0; i < totalRows; i++) {
                 // محاسبه شماره گروه
                 let groupNum = sectionCount.value - Math.floor((i + 1) / rowsPerGroup);
                 if (groupNum < 1) groupNum = 1;
@@ -142,7 +270,7 @@ function sortExcelAndCreateNewExcel(file, recency, frequency, monetary, valuable
                 return 0;
             });
 
-            for(let i= 0; i < totalRows; i++) {
+            for (let i = 0; i < totalRows; i++) {
 
                 // محاسبه شماره گروه
                 let groupNum = sectionCount.value - Math.floor((i + 1) / rowsPerGroup);
@@ -204,28 +332,4 @@ function readExcelFileForGetHeaders(file) {
 
     });
 
-}
-
-function fillSelectTags(headers) {
-
-    headers.forEach(function (h, i) {
-
-        let col = XLSX.utils.encode_col(i);
-
-        recencySelect.appendChild(
-            new Option(h, col)
-        );
-
-        frequencySelect.appendChild(
-            new Option(h, col)
-        );
-
-        monetarySelect.appendChild(
-            new Option(h, col)
-        );
-
-        valuableSelect.appendChild(
-            new Option(h, col)
-        );
-    });
 }
